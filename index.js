474e12470de8ae5246d06f4bf783fad51d52fb2b
Mike Frawley <frawl021@gmail.com>
2014-11-06 19:32:44 -0600

    better error handling, add command config option
    The config option allows you to configure where slimrb is,
    useful if you cant get slimrb on your PATH for whatever reason.
    
    Added tests for error cases.
    
    Avoid calling done() multiple times, which the tests revealed

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 var util = require('util');
 var spawn = require('child_process').spawn;
 
-function createSlimPreprocessor(logger) {
-  var log = logger.create('preprocessor.slim');
+function createSlimPreprocessor(config, logger) {
+  var log, slimCommand;
+
+  config = config || {};
+  log = logger.create('preprocessor.slim');
+  slimCommand = config.command || 'slimrb';
 
   return function(content, file, done) {
-    var child, html, origPath;
+    var child, html, path, hadError;
 
     log.debug('Processing "%s".', file.originalPath);
 
-    origPath = file.originalPath;
-    html = '';
-    child = spawn("slimrb", [origPath]);
+    path = file.originalPath;
+    html = ''
+    child = spawn(slimCommand, [path]);
 
+    // Handle an error in the command, such as slimrb not found
     child.on('error', function (error) {
-      if (error.code == 'ENOENT') {
-        log.error("It does not appear as though `slimrb` is installed. Try running `bundle install` in your project.");
-        done('');
+      if (error.code === 'ENOENT') {
+        log.error(
+          "It does not appear as though `slimrb` is installed.\n" +
+          "You may need to run `bundle install` in your project, or install it globally.\n" +
+          "If you're having PATH issues you can configure where slimrb, inside your karma config file:\n\t" +
+          "slimPreprocessor: { command: '/usr/local/slimrb' }`"
+        );
+      }
+      hadError = true;
+      done(null);
+    });
+
+    // Handle an error from stderr, which would be a slim syntax error
+    child.stderr.on('data', function (buf) {
+      // We already had a command error, don't call done twice
+      if(!hadError) {
+        hadError = true;
+        done("Error compiling slim template '" + path + "'" + String(buf));
       }
     });
 
@@ -25,16 +45,16 @@ function createSlimPreprocessor(logger) {
     });
 
     child.stdout.on('close', function (buf) {
-      done(html);
+      // Don't call done again if we had a syntax error
+      if(!hadError) {
+        done(html);
+      }
     });
 
-    child.stderr.on('data', function (buf) {
-      throw String(buf);
-    });
   };
 }
 
-createSlimPreprocessor.$inject = ['logger'];
+createSlimPreprocessor.$inject = ['config.slimPreprocessor', 'logger'];
 
 module.exports = {
   'preprocessor:slim': ['factory', createSlimPreprocessor]
--- test/slim2html.spec.coffee
+++ test/slim2html.spec.coffee
@@ -2,13 +2,17 @@ describe 'preprocessors slim2html', ->
   chai = require('chai')
   templateHelpers = require('./helpers/template_cache')
   slim2html = require('../index')['preprocessor:slim'][1]
-  console.log slim2html
+  {which} = require('shelljs')
 
   chai.use(templateHelpers)
   {expect} = chai
 
-  logger = create: -> {debug: ->}
-  process = null
+  slimCommand = which 'slimrb'
+  if !slimCommand
+    throw new Error("Cannot find a slimrb on your path, aborting tests.")
+
+  noop = ->
+  logger = create: -> {debug: noop, info: noop, error: noop}
 
   # TODO(vojta): refactor this somehow ;-) it's copy pasted from lib/file-list.js
   File = (path, mtime) ->
@@ -18,17 +22,32 @@ describe 'preprocessors slim2html', ->
     @mtime = mtime
     @isUrl = false
 
-  createPreprocessor = () ->
-    slim2html logger
 
-  beforeEach ->
-    process = createPreprocessor()
-
-  it 'should convert html to js code', (done) ->
+  it 'converts slim to html', (done) ->
     file = new File 'test/fixtures/index.slim'
-
-    process '', file, (processedContent) ->
-      expect(processedContent).to.equal('<h1>Hello</h1><div class="dece">And welcome!</div>\n')
+    process = slim2html({}, logger)
+    process '', file, (result) ->
+      expect(result).to.equal('<h1>Hello</h1><div class="dece">And welcome!</div>\n')
       done()
 
+  it 'throws an error given invalid slim markup', (done) ->
+    file = new File 'test/fixtures/invalid.slim'
+    process = slim2html({}, logger)
+    process '', file, (result) ->
+      expect(result).to.match(/SyntaxError: Malformed indentation/)
+      done()
+
+  it 'allows configuring the path of the slimrb binary', (done) ->
+    file = new File 'test/fixtures/index.slim'
+    process = slim2html({ command: slimCommand }, logger)
+    process '', file, (result) ->
+      expect(result).to.equal('<h1>Hello</h1><div class="dece">And welcome!</div>\n')
+      done()
+
+  it 'throws an error given an invalid slim command configuration', (done) ->
+    file = new File 'test/fixtures/index.slim'
+    process = slim2html({ command: "foooooooooooo" }, logger)
+    process '', file, (result) ->
+      console.log('done')
+      done()
 
